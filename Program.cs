2543b5efec277ee014d4f68b3bddc081c5b61806
NovaCore <novacore89@gmail.com>
2019-10-22 16:13:56 -0700

    Upload Drill 34

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're 'accounting' on you!");
+            Console.Read();
+
+        }   
+         
+    }
+}
