77e798ac75cf24f609584315cdfb3f25a994c5bd
NovaCore <novacore89@gmail.com>
2019-11-11 15:27:06 -0800

    Upload drill 113

---
+++ DRILL_113_Class_Methods/DRILL_113_Class_Methods/Integers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_113_Class_Methods
+{
+    class Integers
+    {
+        public void Add(int num, int num2)
+        {
+            Console.WriteLine(50 + num);
+            Console.WriteLine(num2);
+        }
+    }
+}
+
---
+++ DRILL_113_Class_Methods/DRILL_113_Class_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_113_Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Choose an whole integer to add 50 to.");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose another integer to display.");
+            int inputTwo = Convert.ToInt32(Console.ReadLine());
+
+            Integers numbers = new Integers();
+            numbers.Add(inputOne, inputTwo);
+            numbers.Add(num: inputOne, num2: inputOne);
+
+        
+
+             Console.ReadLine();
+        }
+    }
+}
