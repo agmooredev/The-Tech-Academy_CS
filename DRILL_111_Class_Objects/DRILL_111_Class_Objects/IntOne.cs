cc6861f071cfdb8779e6bc8760a2a7d393b3823d
NovaCore <novacore89@gmail.com>
2019-11-11 13:42:09 -0800

    Upload drill 111

---
+++ DRILL_111_Class_Objects/DRILL_111_Class_Objects/IntOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_111_Class_Objects
+{
+    class IntOne
+
+    { 
+        public int AddTen(int a)
+        {
+            return a + 10;
+        }
+
+        public int SubtractTen(int a)
+        {
+            return a - 10;
+
+        }
+
+        public int MultiplybyTen(int a)
+        {
+            return a * 10;
+        }
+    }
+}
---
+++ DRILL_111_Class_Objects/DRILL_111_Class_Objects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_111_Class_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Choose a whole integer you would like to do math operations with.");
+                int inputNum = Convert.ToInt32(Console.ReadLine());
+
+                IntOne numberOne = new IntOne();
+                Console.WriteLine(numberOne.AddTen(inputNum));
+                Console.WriteLine(numberOne.SubtractTen(inputNum));
+                Console.WriteLine(numberOne.MultiplybyTen(inputNum));
+
+
+
+
+                Console.ReadLine();
+            }
+    }
+}
