4a52d8ecbfcccc9f94b0e76e95ff3aaa8a3f4e1e
NovaCore <novacore89@gmail.com>
2019-11-11 17:17:36 -0800

    Upload drill 115

---
+++ DRILL_115_Class_Methods(OptionalParameters)/DRILL_115_Class_Methods(OptionalParameters)/Integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_115_Class_Methods_OptionalParameters_
+{
+    class Integer
+    {
+        public int Add(int num, int num2 = 10)
+        {
+            return (num + num2);
+            
+        }
+    
+      
+    }
+}
---
+++ DRILL_115_Class_Methods(OptionalParameters)/DRILL_115_Class_Methods(OptionalParameters)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_115_Class_Methods_OptionalParameters_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole integer.");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose a 2nd integer (optional).");
+            string inputOptional = Console.ReadLine();
+
+            Integer method = new Integer();
+
+            if (int.TryParse(inputOptional, out int optInteger))
+            {
+                
+                Console.WriteLine(method.Add(optInteger, inputOne));
+                }
+
+            else
+            { 
+            Console.WriteLine(method.Add(inputOne));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
