fc20e397af384faf71b43ef7ac0a5989c05bb89c
NovaCore <novacore89@gmail.com>
2019-11-11 16:02:41 -0800

    Upload drill 114

---
+++ DRILL_114_Class_Methods/DRILL_114_Class_Methods/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_114_Class_Methods
+{
+    class Methods
+    {
+        public int defaultMethod(int num) 
+        {
+            int result = num + 42; 
+            return result;
+        }
+
+        public int defaultMethod(decimal num) 
+        {
+            int result = Convert.ToInt32(num) + 485; 
+            return result;
+        }
+
+        public int defaultMethod(string num) 
+        {
+            int result = Convert.ToInt32(num) + 64; 
+            return result;
+        }
+    }
+}
---
+++ DRILL_114_Class_Methods/DRILL_114_Class_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_114_Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CLASS
+            Methods method = new Methods();
+
+            //CALL METHOD
+            Console.WriteLine("The integer 10, will have 42 added.\n" + method.defaultMethod(10));
+
+            Console.WriteLine("The decimel 349.50 will be converted to an integer and have 345 added to it. \n" + method.defaultMethod(349.50m));
+
+            Console.WriteLine("The string value of 54 will be converted to an integer, and have 64 added to it.\n" + method.defaultMethod("54"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
