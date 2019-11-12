11ddc3c08e228a78d3eb8f00abf2b917761365aa
NovaCore <novacore89@gmail.com>
2019-11-12 13:46:57 -0800

    Upload drill 117

---
+++ DRILL_117_Class_Instantiate/DRILL_117_Class_Instantiate/Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_117_Class_Instantiate
+{
+    class Method
+    {
+        public void divByTwo(int num) //VOID METHOD OUTPUTING INT
+        {
+            Console.WriteLine(num / 2); //DIVIDE INT BY TWO
+        }
+
+        public void Output(out int a, out int b) //METHOD WITH OUTPUT PARAMETERS
+        {
+            Console.WriteLine("Enter a value to be assigned to 'a' parameter.");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value to be assigned to 'b' parameter.");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        public static void Overload(int num)
+        {
+            Console.WriteLine("Overload(int)");
+        }
+
+        public static void Overload(string text)
+        {
+            Console.WriteLine("Overload(string)");
+        }
+    }
+}
---
+++ DRILL_117_Class_Instantiate/DRILL_117_Class_Instantiate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_117_Class_Instantiate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose an integer to divide by 2");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            Method num = new Method(); //INSTANTIATE CLASS
+            num.divByTwo(inputNum); // DIVIDE INPUT BY 2
+            Console.WriteLine("Press enter to continue..");
+            Console.ReadLine();
+
+            Method num2 = new Method(); //2ND INSTANTIATION FOR CLASS "INTEGER"
+            int a, b;
+            num2.Output(out a, out b); //OUTPUT PARAMATERS
+            Console.WriteLine("The Value of a is: {0}", a);
+            Console.WriteLine("The value of b is: {0}", b);
+
+            Console.WriteLine("Press enter to continue..");
+            Console.ReadLine();
+
+            Method.Overload("string");
+            Console.ReadLine();
+        }
+    }
+}
