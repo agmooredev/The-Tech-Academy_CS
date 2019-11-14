889d6891d9bcc6319e67136eeee05d4d74e1542e
NovaCore <novacore89@gmail.com>
2019-11-13 17:55:09 -0800

    Update drill 120 and upload drill 124

--- DRILL_124_Inheritance_Abstract/DRILL_124_Inheritance_Abstract/Properties/AssemblyInfo.cs
+++ DRILL_124_Inheritance_Abstract/DRILL_124_Inheritance_Abstract/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DRILL_124_Inheritence")]
+[assembly: AssemblyTitle("DRILL_124_Inheritance_Abstract")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DRILL_124_Inheritence")]
+[assembly: AssemblyProduct("DRILL_124_Inheritance_Abstract")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c8996bf-6bf6-45c8-a30a-910ab6fcefe5")]
+[assembly: Guid("c624af21-5fa0-49c0-bc70-989c533274de")]
 
 // Version information for an assembly consists of the following four values:
 //
