973b8fa3de68a84667a475d35cb5dbdd90e5ac11
NovaCore <novacore89@gmail.com>
2019-11-20 14:03:34 -0800

    Upload drill 137

---
+++ DRILL_137_Structures/DRILL_137_Structures/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_137_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                    Number num = new Number(); //NUMBER OBJECT IN MAIN()
+                    num.Amount = 10000; //AMOUNT
+                    Console.WriteLine(num.Amount); //PRINT AMOUNT NUMBER TO CONSOLE
+                    Console.Read();
+
+             }
+
+
+
+
+
+
+         }
+    }
+
