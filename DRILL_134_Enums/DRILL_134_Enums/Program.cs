3a1c9e86a5fca474157d8adae5987d6cce2ec38b
NovaCore <novacore89@gmail.com>
2019-11-20 13:22:14 -0800

    Upload drill 134

--- DRILL_134_Enums/DRILL_134_Enums/Program.cs
+++ DRILL_134_Enums/DRILL_134_Enums/Program.cs
@@ -8,29 +8,50 @@ namespace DRILL_134_Enums
 {
     class Program
     {
-          //ENUM FOR DAYS OF THE WEEK
+        //ENUM FOR DAYS OF THE WEEK
         public enum DayofWeek { monday, tuesday, wednesday, thursday, friday, saturday, sunday };
 
         static void Main(string[] args)
         {
+
+            start:
             try // TRY/CATCH BLOCK
             {
                 Console.WriteLine("Enter the current day of the week"); //PROMPT USER
                 string currentDay = Console.ReadLine();
-                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), currentDay, true); //ASSIGN VAL TO VAR OF ENUM
-                Console.WriteLine("Today is " + day);
+
+                if (int.TryParse(currentDay, out int x))
+                {
+                    throw new System.Exception("You cannot enter a number value");
+                    goto start;
+                    //Console.WriteLine("Please enter a number");
+                }
+
+                else
+                {
+
+                    DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), currentDay, true); //ASSIGN VAL TO VAR OF ENUM
+                    Console.WriteLine("Today is " + day);
+                }
             }
 
             catch (Exception ex)  //PRINT ERROR FOR INCORRECT DATA ENTRY
             {
                 Console.WriteLine("Error. Please enter an actual day of the week");
+                Console.WriteLine(ex);
+                goto start;
             }
 
             finally
             {
                 Console.ReadLine();
+
+ 
             }
         }
     }
 }
+        
+    
+
 
