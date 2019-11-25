0d3cbef4ddb0566bdc84b72f00594ae9460fc7df
NovaCore <novacore89@gmail.com>
2019-11-25 13:08:06 -0800

    Upload twenty onw game 1st draft

---
+++ TwentyOneGame/TwentyOneGame/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOneGame
+{
+    public class Card
+    {
+        public class Card
+        {
+            public string Suit { get; set; }
+            public string Face { get; set; }
+        }
+    }
+}
---
+++ TwentyOneGame/TwentyOneGame/TwentyOneGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOneGame
+{
+    public class TwentyOneGame : Game, IWalkAway
+    {
+        public TwentyOneDealer Dealer { get; set; }
+        public override void Play()
+        {
+            Dealer = new TwentyOneDealer();
+            foreach (Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            Console.WriteLine("Place your bet!");
+
+            foreach (Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+                bool successfullyBet = player.Bet(bet);
+                if (!successfullyBet)
+                {
+                    return;
+                }
+                Bets[player] = bet;
+            }
+
+
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
