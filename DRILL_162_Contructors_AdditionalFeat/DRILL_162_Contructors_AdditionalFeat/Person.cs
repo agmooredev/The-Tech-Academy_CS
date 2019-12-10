e5adda1f61d152cd8c3ac84f2847778776e9916a
NovaCore <novacore89@gmail.com>
2019-12-10 12:32:42 -0800

    Upload drill 162

---
+++ DRILL_162_Contructors_AdditionalFeat/DRILL_162_Contructors_AdditionalFeat/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_162_Contructors_AdditionalFeat
+{
+ 
+    
+        public class Person
+        {
+            //CONST VARIABLE
+            const string colorOfSky = "Blue";
+
+
+            public string First;
+            public string Last;
+            public int Age;
+
+            //CHAIN 2 CONSTRUCTORS TOGETHER
+            public Person(string name) : this(name, "-", 0)
+            {
+                var First = name; 
+            }
+
+            public Person(string name, string surname) : this(name, surname, 0)
+            {
+                First = name;
+                Last = surname;
+            }
+
+            public Person(string name, string surname, int integer)
+            {
+                First = name;
+                Last = surname;
+                Age = integer;
+            }
+
+            public void printName()
+            {
+                Console.WriteLine(First + " " + Last + "," + Age);
+            }
+        }
+    }
+
+
---
+++ DRILL_162_Contructors_AdditionalFeat/DRILL_162_Contructors_AdditionalFeat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRILL_162_Contructors_AdditionalFeat
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Person ex = new Person("John", "Smith", 27);
+                ex.printName();
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    
